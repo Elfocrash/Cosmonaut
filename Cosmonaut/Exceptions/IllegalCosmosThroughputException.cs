7b62a48b7be74eaa56bb48f994cd659c59ba117d
Nick Chapsas <elfocrash@gmail.com>
2018-04-07 17:53:19 +0100

    Added automatic upscaling for huge requests

--- Cosmonaut/Exceptions/IllegalCosmosThroughputException.cs
+++ Cosmonaut/Exceptions/IllegalCosmosThroughputException.cs
@@ -4,7 +4,7 @@ namespace Cosmonaut.Exceptions
 {
     public class IllegalCosmosThroughputException : Exception
     {
-        public IllegalCosmosThroughputException() : base("CosmosDB throughput cannot be less than 400.")
+        public IllegalCosmosThroughputException() : base("CosmosDB throughput cannot be less than 400 and has to be incrementals of 100.")
         {
 
         }
